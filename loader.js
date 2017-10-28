c5f1de03d41b0212177d23ce42f6f27557d2ee7c
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-28 14:43:48 -0700

    Loader/script split to work around TM require script caching.

---
+++ loader.js
@@ -0,0 +1,9 @@
+window.WKHelper = window.WKHelper || {};
+window.WKHelper.init_callbacks = window.WKHelper.init_callbacks || [];
+window.WKHelper.init = function(gm_info, callback) {
+    init_callbacks.push({info: gm_info, callback: callback})
+}
+
+var scriptElement = document.createElement('script');
+scriptElement.setAttribute("type","text/javascript")
+scriptElement.setAttribute("src", "https://raw.githubusercontent.com/curious-attempt-bunny/wanikani-user-script-helper/master/script.js")
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.WKHelper = {
-    test: "Hello world, I updated!"
-};
+if (!window.WKHelper || !window.WKHelper.init_callbacks) {
+    console.log("WKHelper must be loaded via the loader.js script.");
+    return;
+}
+
+window.WKHelper.init_callbacks.forEach(function(init_callback) {
+    var info = init_callback.info;
+    var callback = init_callback.callback;
+
+    console.log(info, callback);
+});
