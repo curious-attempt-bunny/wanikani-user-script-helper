b8929f311ae610c95542aa18931046631534e0ae
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-28 15:42:34 -0700

    Make init robust to script errors.

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ if (!window.WKHelper || !window.WKHelper.init_callbacks) {
     console.log("WKHelper must be loaded via the loader.js script.");
 } else {
     window.WKHelper.init_callbacks.forEach(function(init_callback) {
-        var info = init_callback.info;
-        var callback = init_callback.callback;
+        var name = 'unidentified';
+        try {
+            var info = init_callback.info;
+            var callback = init_callback.callback;
 
-        console.log(info, callback);
+            name = window.WKHelper.init_callbacks[0].info.script.name;
+            console.log("Starting WKHelper callback for "+name+"...");
+            callback(window.WKHelper);
+            console.log("Completed WKHelper callback for "+name+".");
+        } catch (e) {
+            console.log("Error during WKHelper callback for "+name+".", e);
+        }
     });
 }
