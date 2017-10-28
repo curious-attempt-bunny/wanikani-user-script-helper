a382e238472e6693b2943b323d3408881bb69947
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-28 14:50:39 -0700

    Add debugging info.

--- loader.js
+++ loader.js
@@ -1,10 +1,15 @@
+console.log("Loader running!");
 window.WKHelper = window.WKHelper || {};
 window.WKHelper.init_callbacks = window.WKHelper.init_callbacks || [];
 window.WKHelper.init = function(gm_info, callback) {
     init_callbacks.push({info: gm_info, callback: callback})
 }
 
+console.log(document);
+
+console.log("Creating script element!");
 var scriptElement = document.createElement('script');
-console.dir(scriptElement);
-scriptElement.setAttribute("type","text/javascript")
-scriptElement.setAttribute("src", "https://raw.githubusercontent.com/curious-attempt-bunny/wanikani-user-script-helper/master/script.js")
+console.log("Created script element", scriptElement);
+
+// scriptElement.setAttribute("type","text/javascript")
+// scriptElement.setAttribute("src", "https://raw.githubusercontent.com/curious-attempt-bunny/wanikani-user-script-helper/master/script.js")
