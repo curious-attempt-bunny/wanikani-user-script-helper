54dd8a82b2b21d20848d78e66b57223d960c0934
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-28 15:33:18 -0700

    Fix syntax.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 console.log("WKHelper running!");
 if (!window.WKHelper || !window.WKHelper.init_callbacks) {
     console.log("WKHelper must be loaded via the loader.js script.");
-    return;
+} else {
+    window.WKHelper.init_callbacks.forEach(function(init_callback) {
+        var info = init_callback.info;
+        var callback = init_callback.callback;
+
+        console.log(info, callback);
+    });
 }
-
-window.WKHelper.init_callbacks.forEach(function(init_callback) {
-    var info = init_callback.info;
-    var callback = init_callback.callback;
-
-    console.log(info, callback);
-});
