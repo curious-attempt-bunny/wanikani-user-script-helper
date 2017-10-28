482a18de9029a3bbaab565d31a8b797278e52056
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-28 15:11:00 -0700

    Refine loading logic.

--- loader.js
+++ loader.js
@@ -1,16 +1,19 @@
-console.log("Loader running!");
+console.log("Loader for WKHelper running!");
 window.WKHelper = window.WKHelper || {};
 window.WKHelper.init_callbacks = window.WKHelper.init_callbacks || [];
 window.WKHelper.init = function(gm_info, callback) {
-    init_callbacks.push({info: gm_info, callback: callback})
-}
+    window.WKHelper.init_callbacks.push({info: gm_info, callback: callback})
+};
 
-console.log(document);
-
-console.log("Creating script element!");
-var scriptElement = document.createElement('script');
-console.log("Created script element:");
-console.dir(scriptElement);
-
-// scriptElement.setAttribute("type","text/javascript")
-// scriptElement.setAttribute("src", "https://raw.githubusercontent.com/curious-attempt-bunny/wanikani-user-script-helper/master/script.js")
+(function(d, s, id) {
+  var js, finaljs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    console.log("WKHelper script already appended.");
+    return;
+  }
+  console.log("Appending WKHelper script...");
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//raw.githubusercontent.com/curious-attempt-bunny/wanikani-user-script-helper/master/script.js";
+  finaljs.parentNode.insertBefore(js, finaljs);
+}(document, 'script', 'wanikani-user-script-helper'));
